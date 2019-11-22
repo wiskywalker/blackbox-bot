b4c0200dee64ad9062fa2dd15634939753da3e55
W. A <jwae_123@hotmail.com>
2019-11-22 09:50:59 -0600

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+  if (msg.content === 'hola') {
+    msg.reply('OLA K ASE?');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
